7a16cd4653521a5b809b1c71c1ca1268bd585f1a
Bo Niu <niubo19930402@hotmail.com>
2020-05-13 19:17:56 -0700

    chapter 1 finished

--- public/App.js
+++ public/App.js
@@ -1,7 +1,14 @@
+"use strict";
+
 // const element = React.createElement('div', {title: 'Outer div'},
 //     React.createElement('h1', null, 'Hello World!')
 // );
-const element = /*#__PURE__*/React.createElement("div", {
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+});
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('content'));
