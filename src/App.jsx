91d073158db684f4cdde55ca3626772540434476
Bo Niu <niubo19930402@hotmail.com>
2020-05-13 19:17:56 -0700

    chapter 2 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // const element = React.createElement('div', {title: 'Outer div'},
 //     React.createElement('h1', null, 'Hello World!')
 // );
+
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+const message = helloContinents.join(' ');
+
+
 const element = (
     <div title="Outer div">
-        <h1>Hello World!</h1>
+        <h1>{message}</h1>
     </div>
 );
 ReactDOM.render(element, document.getElementById('content'));
