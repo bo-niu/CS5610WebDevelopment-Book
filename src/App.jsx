3b6239b2f383f6cf4e4bb5c25337f024fb6381cd
Bo Niu <niubo19930402@hotmail.com>
2020-05-21 04:01:12 -0700

    change helloworld into react class

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@
 //     React.createElement('h1', null, 'Hello World!')
 // );
 
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
+class HelloWorld extends React.Component {
+    render() {
+        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+        const message = helloContinents.join(' ');
+        return (
+            <div title="Outer div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
 
 
-const element = (
-    <div title="Outer div">
-        <h1>{message}</h1>
-    </div>
-);
+const element = <HelloWorld />;
 ReactDOM.render(element, document.getElementById('content'));
