eaa61c7ea09133c3c7b5bbb768804a285841ed50
Bo Niu <niubo19930402@hotmail.com>
2020-05-22 17:35:00 -0700

    try to change .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,15 @@
-class IssueFilter extends React.Component {
-    render() {
-        return (
-            <div>This is a placeholder for the issue filter.</div>
-        );
-    }
-}
+// const element = React.createElement('div', {title: 'Outer div'},
+//     React.createElement('h1', null, 'Hello World!')
+// );
+
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+const message = helloContinents.join(' ');
 
 
-class IssueRow extends React.Component {
-    render() {
-        const style = this.props.rowStyle;
-        return (
-            <tr>
-                <td style={style}>{this.props.issue_id}</td>
-                <td style={style}>{this.props.children}</td>
-            </tr>
-        );
-    }
-}
-
-
-
-class IssueTable extends React.Component {
-    render() {
-        const rowStyle = {border: "1px solid silver", padding: 4};
-        return (
-            <table style = {{borderCollapse: "collapse"}}>
-                <thead>
-                    <tr>
-                        <th style={rowStyle}>ID</th>
-                        <th style={rowStyle}>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <IssueRow rowStyle={rowStyle} issue_id={1}>
-                        Error in console when clicking Add
-                    </IssueRow>
-                    <IssueRow rowStyle={rowStyle} issue_id={2}>
-                        <div>Missing <b>bottom</b> border on panel</div>
-                    </IssueRow>
-                </tbody>
-            </table>
-        );
-    }
-}
-
-class IssueAdd extends React.Component {
-    render() {
-        return (
-            <div>This is a placeholder for a form to add an issue.</div>
-        );
-    }
-}
-
-class IssueList extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable />
-                <hr />
-                <IssueAdd />
-            </React.Fragment>
-        );
-    }
-}
-
-
-
-
-const element = <IssueList />;
+const element = (
+    <div title="Outer div">
+        <h1>{message}</h1>
+    </div>
+);
 ReactDOM.render(element, document.getElementById('content'));
