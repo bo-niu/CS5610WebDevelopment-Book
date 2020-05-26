6fc32eb8606c68472a641a4c254b43a397a93c61
bo-niu <niubo0402@hotmail.com>
2020-05-26 00:00:34 -0700

    finished lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -48,36 +48,9 @@ class IssueRow extends React.Component {
 
 class IssueTable extends React.Component {
 
-    constructor(){
-        super();
-        this.state = {issues:[]};
-        setTimeout(()=>{
-            this.createIssue(sampleIssue);
-        }, 2000);
-    }
-
-    createIssue(issue) {
-        issue.id = this.state.issues.length + 1;
-        issue.created = new Date();
-        const newIssueList = this.state.issues.slice();
-        newIssueList.push(issue);
-        this.setState({issues: newIssueList});
-        
-    }
-
-    loadData() {
-        setTimeout(() => {
-            this.setState({issues: initialIssues});
-        }, 1000);
-    }
-
-    componentDidMount() {
-        this.loadData();
-    }
-
     render() {
 
-        const issueRows = this.state.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
         const rowStyle = {border: "1px solid silver", padding: 4};
         return (
             <table className = "bordered-table">
@@ -94,15 +67,6 @@ class IssueTable extends React.Component {
                 </thead>
                 <tbody>
                     {issueRows}
-                    {/* {issues.map(issue => <IssueRow rowStyle={rowStyle} issue={issue}/>)} */}
-
-
-                    {/* <IssueRow rowStyle={rowStyle} issue_id={1}>
-                        Error in console when clicking Add
-                    </IssueRow>
-                    <IssueRow rowStyle={rowStyle} issue_id={2}>
-                        <div>Missing <b>bottom</b> border on panel</div>
-                    </IssueRow> */}
                 </tbody>
             </table>
         );
@@ -110,6 +74,14 @@ class IssueTable extends React.Component {
 }
 
 class IssueAdd extends React.Component {
+
+    constructor() {
+        super();
+        setTimeout(()=>{
+            this.props.createIssue(sampleIssue);
+        }, 2000);
+    }
+
     render() {
         return (
             <div>This is a placeholder for a form to add an issue.</div>
@@ -118,15 +90,40 @@ class IssueAdd extends React.Component {
 }
 
 class IssueList extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {issues:[]};
+        this.createIssue = this.createIssue.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData() {
+        setTimeout(() => {
+            this.setState({issues: initialIssues});
+        }, 500);
+    }
+
+    createIssue(issue) {
+        issue.id = this.state.issues.length + 1;
+        issue.created = new Date();
+        const newIssueList = this.state.issues.slice();
+        newIssueList.push(issue);
+        this.setState({issues: newIssueList});
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h1>Issue Tracker</h1>
                 <IssueFilter />
                 <hr />
-                <IssueTable />
+                <IssueTable issues={this.state.issues}/>
                 <hr />
-                <IssueAdd />
+                <IssueAdd createIssue={this.createIssue}/>
             </React.Fragment>
         );
     }
