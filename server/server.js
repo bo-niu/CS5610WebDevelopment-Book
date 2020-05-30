2bb4bdf77c855d6370d916cee93aab060f9935f5
bo-niu <niubo0402@hotmail.com>
2020-05-29 20:25:11 -0700

    finished graphql schema file

--- server/server.js
+++ server/server.js
@@ -1,41 +1,32 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-
+const {ApolloServer} = require('apollo-server-express');
 let aboutMessage = "Issue Tracker API v1.0";
-
-const typeDefs = `
-  type Query {
-    about: String!
-  }
-  type Mutation {
-    setAboutMessage(message: String!): String
-  }
-`;
+const fs = require('fs');
 
 const resolvers = {
-  Query: {
-    about: () => aboutMessage,
-  },
-  Mutation: {
-    setAboutMessage,
-  },
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
 };
 
-function setAboutMessage(_, { message }) {
-  return aboutMessage = message;
+function setAboutMessage(_, {message}) {
+    return aboutMessage = message;
 }
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers,
 });
 
 const app = express();
 
 app.use(express.static('public'));
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({app, path: '/graphql'});
 
-app.listen(3000, function () {
-  console.log('App started on port 3000');
+app.listen(3000, function(){
+    console.log('App started on port 3000');
 });
