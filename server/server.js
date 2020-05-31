fc488da15156de8c1b13d4e3dbe97e2e9ba7830b
bo-niu <niubo0402@hotmail.com>
2020-05-30 23:17:27 -0700

    finished the list api

--- server/server.js
+++ server/server.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 let aboutMessage = "Issue Tracker API v1.0";
+
+const issuesDB = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('2019-01-15'), due: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+        title: 'Missing bottom borderon panel',
+    }
+];
+
 const fs = require('fs');
 
 const resolvers = {
     Query: {
         about: () => aboutMessage,
+        issueList,
     },
     Mutation: {
         setAboutMessage,
     },
 };
+
+function issueList() {
+    return issuesDB;
+}
 
 function setAboutMessage(_, {message}) {
     return aboutMessage = message;
@@ -23,6 +42,7 @@ const server = new ApolloServer({
 
 const app = express();
 
+//Set static folder
 app.use(express.static('public'));
 
 server.applyMiddleware({app, path: '/graphql'});
