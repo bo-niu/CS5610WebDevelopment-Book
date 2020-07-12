52c13138db5e438c4352defd3f9b16f98dfdc164
bo-niu <niubo0402@hotmail.com>
2020-07-12 01:14:40 -0700

    finished generated routes

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter, matchPath } from 'react-router-dom';
 import template from './template.js';
 import Page from '../src/Page.jsx';
-import About from '../src/About.jsx';
 import store from '../src/store.js';
+import routes from '../src/routes.js';
 
 async function render(req, res) {
-  const initialData = About.fetchData();
+  const activeRoute = routes.find(
+    route => matchPath(req.path, route),
+  );
+
+  let initialData;
+  if (activeRoute && activeRoute.component.fetchData) {
+    initialData = await activeRoute.component.fetchData();
+  }
   store.initialData = initialData;
   const element = (
     <StaticRouter location={req.url} context={{}}>
